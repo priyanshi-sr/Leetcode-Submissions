97b56cdd69d0cf7cd288320c3313dfc3d04031bc
priyanshi-sr <125743381+priyanshi-sr@users.noreply.github.com>
2024-03-19 23:12:19 +0530

    Time: 54 ms (56.60%) | Memory: 49.3 MB (47.32%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,9 +5,9 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let val = init;
-  for (let i = 0; i < nums.length; i++) {
-    val = fn(val, nums[i]);
-  }
-  return val;l̥
+    
+    for(let i = 0; i<nums.length; i++){
+         init = fn(init, nums[i])
+    }
+    return init
 };
