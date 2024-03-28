d13ddc9e87287d1764f22a7453a73a4b41333fcb
priyanshi-sr <125743381+priyanshi-sr@users.noreply.github.com>
2024-03-28 13:19:40 +0530

    Time: 1 ms (20.66%) | Memory: 39.9 MB (89.30%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -3,21 +3,14 @@ class Solution {
         return(Perfect(n));
     }
      public  static boolean Perfect(int n){
-        long Start = 1;
-        long end = n;
-        while(Start<=end){
-            long mid = Start +(end-Start)/2;
-            if((mid*mid)==n){
-                return true;
-            }
-            if(mid*mid>n) {
-                end = mid - 1;
-            }
-            else{
-                Start = mid+1;
-
-            }
-        }
+       if (n < 0) // Negative numbers are not perfect squares
         return false;
+    
+    for (long i = 1; i * i <= n; i++) {
+        if (i * i == n)
+            return true;
+    }
+    
+    return false;
     }
 }
