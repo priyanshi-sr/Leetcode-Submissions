1bf1dfd4eb9191584ec885b9d4c0a0496da26864
priyanshi-sr <125743381+priyanshi-sr@users.noreply.github.com>
2024-09-04 12:33:08 +0530

    Time: 4 ms (73.25%) | Memory: 43.8 MB (99.37%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,39 +9,27 @@
  * }
  */
 class Solution {
-  public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
-
-        int step = 1;
-        while (step < lists.length) {
-            for (int i = 0; i < lists.length - step; i += step * 2) {
-                lists[i] = merge2Lists(lists[i], lists[i + step]);
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>(){
+            public int compare(ListNode o1,ListNode o2){
+                return o1.val - o2.val;
             }
-            step *= 2;
-        }
-        return lists[0]; // Return the merged list head
-    }
-
-    private ListNode merge2Lists(ListNode head1, ListNode head2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode prev = dummy;
-
-        // Traverse over the lists until both or one of them becomes null
-        while (head1 != null && head2 != null) {
-            if (head1.val <= head2.val) {
-                prev.next = head1; // Add l1 node to the list
-                head1 = head1.next;
-            } else {
-                prev.next = head2; // Add l2 node to the list
-                head2 = head2.next;
+        });
+        for(ListNode ll: lists){
+            if(ll !=null){
+                pq.add(ll);
             }
-            prev = prev.next; // Move the prev pointer forward
         }
-
-        // If either list still has nodes left, append them
-        if (head1 != null) prev.next = head1;
-        else prev.next = head2;
-
-        return dummy.next; // Return the merged list
+        ListNode Dummy = new ListNode();
+        ListNode temp = Dummy;
+        while(!pq.isEmpty()){
+            ListNode rv = pq.poll();
+            Dummy.next = rv;
+            Dummy = Dummy.next;
+            if(rv.next!= null){
+                pq.add(rv.next);
+            }
+        }
+        return temp.next;
     }
 }
